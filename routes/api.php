eae356bdfbc1b89dbfbbe96194d43ff5f85e6f69
KAMOUN SAMAR <samarkamoun98@gmail.com>
2024-11-27 21:36:17 +0100

    fixed auth

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\AuthController;
 
-Route::post('register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/validate-token', [AuthController::class, 'validateToken']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/user', [AuthController::class, 'user']);
 Route::post('/logout', [AuthController::class, 'logout']);
